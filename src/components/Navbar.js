df4059a3fdbdcc294791ef34bf2de760de3eb3f4
Laksh Gupta <lakshgupta253@gmail.com>
2024-05-26 02:58:28 +0530

    Rollback Navbar & Projects Component to avoid code break

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,29 @@
+// src/components/Navbar.js
+
 import { ArrowRightIcon } from "@heroicons/react/solid";
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Navbar() {
-  // Function to handle smooth scrolling to target
-  const smoothScroll = (targetId) => {
-    const target = document.querySelector(targetId);
-    if (target) {
-      const navbarHeight = document.querySelector('header').offsetHeight;
-      window.scrollTo({
-        top: target.offsetTop - navbarHeight,
-        behavior: 'smooth'
-      });
-    }
-  };
-
-  useEffect(() => {
-    const hashChangeHandler = () => {
-      if (window.location.hash) {
-        const targetId = `#${window.location.hash.substring(1)}`;
-        smoothScroll(targetId);
-      }
-    };
-
-    window.addEventListener('hashchange', hashChangeHandler);
-    return () => {
-      window.removeEventListener('hashchange', hashChangeHandler);
-    };
-  }, []);
-
-  // Function to handle link click
-  const handleLinkClick = (targetId) => {
-    if (window.location.hash !== targetId) {
-      smoothScroll(targetId);
-    }
-  };
-
   return (
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <div className="title-font font-medium text-white mb-4 md:mb-0">
+        <a className="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Laksh Gupta
           </a>
           <span className="ml-3 text-gray-400 text-sm">@cyborglaksh</span>
-        </div>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-          <a href="#socials" className="mr-5 hover:text-white" onClick={() => handleLinkClick('#socials')}>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+        <a href="#socials" className="mr-5 hover:text-white">
             Socials
           </a>
-          <a href="#projects" className="mr-5 hover:text-white" onClick={() => handleLinkClick('#projects')}>
+          <a href="#projects" className="mr-5 hover:text-white">
             Projects
           </a>
-          <a href="#workex" className="mr-5 hover:text-white" onClick={() => handleLinkClick('#workex')}>
+          <a href="#workex" className="mr-5 hover:text-white">
             Workex
           </a>
-          <a href="#skills" className="mr-5 hover:text-white" onClick={() => handleLinkClick('#skills')}>
+          <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
           {/* <a href="#testimonials" className="mr-5 hover:text-white">
@@ -63,9 +32,8 @@ export default function Navbar() {
         </nav>
         <a
           href="#contact"
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
-        >
-          Hire me
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
 import { projects } from "../data";
 
 export default function Projects() {
-  const [hoveredProject, setHoveredProject] = useState(null);
-
   return (
     <section id="projects" className="text-gray-400 bg-gray-900 body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
@@ -15,42 +12,30 @@ export default function Projects() {
             Apps I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Explore some of the projects I've built and experimented with, showcasing my hands-on experience with various technologies.
+            Hands on Experience & personal projects tried out on new technology.
           </p>
         </div>
-        <div className={`grid md:grid-cols-${Math.min(3, projects.length)} gap-4`}>
-          {projects.map((project, index) => (
+        <div className="flex flex-wrap -m-4">
+          {projects.map((project) => (
             <a
               href={project.link}
               key={project.image}
-              className="w-full"
-              onMouseEnter={() => setHoveredProject(index)}
-              onMouseLeave={() => setHoveredProject(null)}
-            >
-              <div className="bg-gray-800 p-6 rounded-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-lg">
+              className="sm:w-1/2 w-100 p-4">
+              <div className="flex relative">
                 <img
-                  alt="project"
-                  className="object-cover object-center h-64 mb-6 rounded-md"
+                  alt="gallery"
+                  className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <h2 className="text-sm text-green-400 tracking-wider title-font font-medium mb-2">
-                  {project.subtitle}
-                </h2>
-                <h1 className="text-lg text-white font-medium mb-4">
-                  {project.title}
-                </h1>
-                {hoveredProject === index && (
-                  <p className="leading-relaxed text-gray-400 mb-3">
-                    {project.description}
-                  </p>
-                )}
-                <a href={project.link} className="text-indigo-500 inline-flex items-center">
-                  Learn More
-                  <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none">
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </a>
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                    {project.subtitle}
+                  </h2>
+                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                    {project.title}
+                  </h1>
+                  <p className="leading-relaxed">{project.description}</p>
+                </div>
               </div>
             </a>
           ))}
